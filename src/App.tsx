b09154f73496277e8456f8e6cbcc2cc57ac51f0e
MujuniEli <elijahmujuni@gmail.com>
2023-07-28 08:44:55 +0300

    Added log in page to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import './App.css'
 import { Main } from './pages/Main'
+import { Login } from './pages/Login'
 
 function App() {
   
