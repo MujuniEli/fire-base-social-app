87fb1b8ca2490152b3dc1ccad624af70b91bf82b
MujuniEli <elijahmujuni@gmail.com>
2023-07-29 10:53:27 +0300

    Added Nav sign in & out  functionality with firebase

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,28 @@
 import { Link } from "react-router-dom"
+import { auth } from "../config/firebase"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { signOut } from "firebase/auth"
 
 export const Nav = () => {
+    const [user] = useAuthState(auth);
+
+    const userSignOut = async () => {
+        await signOut(auth);
+    };
     return (
             <div className="nav">
                 <Link to="/"> Home </Link>
                 <Link to="/login"> Login </Link>
+
+                <div>
+                    { user && (
+                        <>
+                    <p> {user?.displayName} </p>
+                    <img src={auth.currentUser?.photoURL || ""} alt="" height={30} width={30}/>
+                    <button onClick={userSignOut}> Log Out </button>
+                        </>
+                    )}
+                </div>
             </div>
     )
 }
