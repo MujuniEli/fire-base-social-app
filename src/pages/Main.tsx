50353f9d1c4dcfd29ff68eb36438918d3566eb8c
MujuniEli <elijahmujuni@gmail.com>
2023-07-31 07:40:48 +0300

    Added posts state to main.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,9 @@
+import { getDocs, collection } from "firebase/firestore";
+import { database } from "../config/firebase";
+
 export const  Main = () => {
+    const [postsList, setPostsList] = useState(null)
+    const postRef = collection(database, "posts");
     return (
             <div>
                 <h4>main page</h4>
