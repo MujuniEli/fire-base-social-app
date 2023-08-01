760970d5d7874ac0a094161c7f9052904076803d
MujuniEli <elijahmujuni@gmail.com>
2023-08-01 07:57:30 +0300

    likes functionality edits

--- src/pages/main/Posts.tsx
+++ src/pages/main/Posts.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { auth, database } from "../../config/firebase";
 import { Post as Ipost } from "./Main"
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 interface Props {
     post: Ipost;
@@ -13,14 +14,23 @@ export const Post = (props: Props) => {
 
     const likesRef = collection(database, "likes");
 
+    const likesDoc = query(likesRef, where("postId", "==", post.id))
+
+    const getLikes = async () => {
+       const data = await getDocs(likesDoc)
+       console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+    }
+
     const addLike = async () => {
         await addDoc(likesRef, {
             userId: user?.uid,
             postId: post.id
         });
+    };
 
-        
-    }
+    useEffect(() => {
+        getLikes();
+    }, []);
 
     return (
                 <div>
